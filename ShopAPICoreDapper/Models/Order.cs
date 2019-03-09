8de790440573560ec2755809cb215fc8c29821e1
Serhii Maksimchuk <smakdealcase@gmail.com>
2019-03-09 23:23:51 +0200

    ShopAPICoreDapper project. Added models.

---
+++ ShopAPICoreDapper/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+
+namespace ShopAPICoreDapper.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public string Item { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
