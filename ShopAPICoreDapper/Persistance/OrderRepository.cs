1540ac6e094e01a20d8d3a16e5e77a5ccb669fe0
Serhii Maksimchuk <smakdealcase@gmail.com>
2019-03-10 00:10:44 +0200

    Implemented OrderRepository with simple GetAll method.

---
+++ ShopAPICoreDapper/Persistance/OrderRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShopAPICoreDapper.Core;
+using ShopAPICoreDapper.Models;
+using Dapper;
+
+namespace ShopAPICoreDapper.Persistance
+{
+    public class OrderRepository : IRepository<Order>
+    {
+        private readonly IOracleDbConnection oracleConnection;
+
+        public OrderRepository(IOracleDbConnection oracleConnection)
+        {
+            this.oracleConnection = oracleConnection;
+        }
+
+        public async Task<IEnumerable<Order>> GetAllAsync()
+        {
+            using (var connection = this.oracleConnection.GetConnection())
+            {
+                string query = "SELECT * FROM Orders";
+                connection.Open();
+                var result = await connection.QueryAsync<Order>(query);
+                return result.ToList();
+            }
+        }
+    }
+}
