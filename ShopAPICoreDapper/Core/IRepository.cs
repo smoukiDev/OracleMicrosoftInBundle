e698322e407b1e4f0a9c5b1c1bd4b041e510d39c
Serhii Maksimchuk <smakdealcase@gmail.com>
2019-03-10 00:10:15 +0200

    Designed generic repository interface.

---
+++ ShopAPICoreDapper/Core/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ShopAPICoreDapper.Core
+{
+    public interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        //Task<T> GetByIdAsync(int id);
+        //Task<T> AddAsync(T item);
+        //Task<T> UpdateAsync(int id, T item);
+        //Task<T> DeleteAsync(int id);
+    }
+}
