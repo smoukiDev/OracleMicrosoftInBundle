664e2fdd9d19855ba341166f0a3d2928da899cc7
Serhii Maksimchuk <smakdealcase@gmail.com>
2019-03-10 13:46:33 +0200

    Copy Models, Core, Persistance from API project to Library project.

---
+++ ShopOracleEFDataAccess/Models/ORDER.cs
@@ -0,0 +1,20 @@
+﻿namespace ShopAPIEntityFrameworkSix.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("SHOP.ORDERS")]
+    public partial class ORDER
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ID { get; set; }
+
+        [StringLength(200)]
+        public string ITEM { get; set; }
+
+        public decimal? PRICE { get; set; }
+    }
+}
